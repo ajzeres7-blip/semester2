d218d62651838c3c9260f6c46f1a134338026962
Ice <ajzeres7@gmail.com>
2026-02-28 15:50:58 +0500

    Initial clean upload of all practices

--- practice 4/math1.py
+++ practice 4/math1.py
@@ -1,4 +1,4 @@
 import math
 num=int(input("Input degree: "))
 rad=math.radians(num)
-print("Output: ", rad)
+print("Output:  ", rad)
---
+++ practice 4/math2.py
@@ -0,0 +1,5 @@
+import math
+height=int(input("Height: "))
+base1=int(input("Base 1: "))
+base2=int(input("Base 2: "))
+print("Output area: ", (base1+base2)*height/2)
