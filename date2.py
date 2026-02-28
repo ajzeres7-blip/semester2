07c78279d2c4c42998b5cee312f4dbe76b4be03b
Ice <ajzeres7@gmail.com>
2026-02-28 14:46:06 +0500

    Delete date2.py

--- date2.py
+++
@@ -1,8 +0,0 @@
-import datetime
-x=datetime.datetime.now()
-y=datetime.timedelta(days=1)
-yesterday=x-y
-tommorrow=x+y
-print(yesterday.strftime("%Y-%B-%d"))
-print(x.strftime("%Y-%B-%d"))
-print(tommorrow.strftime("%Y-%B-%d"))
