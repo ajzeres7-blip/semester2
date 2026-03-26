3bf51a9af6f0302c0c8c48a19f57bad28ace2a1b
Ice <ajzeres7@gmail.com>
2026-03-26 21:45:38 +0500

    postgresql

---
+++ practice 7/config.py
@@ -0,0 +1,11 @@
+host="localhost"
+database="phonebook" 
+#"phonebook' made through cmd
+#codes: 
+#psql -U postgres
+#CREATE DATABASE phonebook
+#should be output:CREATE DATABASE
+#\l
+#\l allows to see  list of databases & phonebook should be there
+user="postgres"
+password="25689733"
